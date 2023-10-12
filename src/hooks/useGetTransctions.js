36280da991f40034554cec4490fd67f4ce119f9d
mikofranc <m.ogbechie@native.semicolon.africa>
2023-10-12 23:41:26 +0100

    i created the database(transaction) in firebase

---
+++ src/hooks/useGetTransctions.js
@@ -0,0 +1,7 @@
+import { useState } from "react"
+export const useGetTransactions = ()=>{
+    const [transactions, setTransactions]= useState([]);
+
+    const setTransactions = async ()=>{}
+    return{transactions}
+}
